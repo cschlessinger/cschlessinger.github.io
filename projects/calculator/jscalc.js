57cae7279b1ecd35be02fb8ca0082b77a4750387
Claire Schlessinger <claire.schlessinger@gmail.com>
2015-09-05 00:07:31 -0700

    js calc get screen display concat working, clear button working, attempt arithmetic

--- projects/calculator/jscalc.js
+++ projects/calculator/jscalc.js
@@ -1,28 +1,82 @@
+// Pseudocode:
 
+// Concatenate .innerHTML of button click to string that represents the full, multi-digit number stored as the first argument
+// Store arithmetic function to be applied when = is pressed
+// Concatenate .innerHTML of button click to string that represents the full, multi-digit number stored as the second argument
+// Use = to execute stored arithmetic function with stored arguments
+// Update screen output with result
 
-var fullCalcNum = "";
+// config, (global variables)
+var value = ""
+var operator = null
+var initialOperand = value;
+var subsequentOperand = value;
 
-function sum(a, b) {
-	return a+b;
+// model
+  function sum(a, b) {
+    value = a+b;
+  }
+
+  function subtract(a, b) {
+    value = a-b;
+  }
+
+  function multiply(a, b) {
+    value = a*b;
+  }
+
+  function divide(a, b) {
+    value = a/b;
+  }
+
+  
+  function clearValue() {
+    value = "";
+  }
+
+function concatNumInputs(button) {
+  value += button.innerHTML
+//   console.log(input)
 }
-function subtract(a, b) {
-	return a-b;
+
+// controller
+$('.num').on('click', function() { 
+  var button = this;
+  concatNumInputs(button);
+  displayNumOnScreen();
+});
+
+$('#clearButton').on('click', function() { 
+  clearValue();
+  displayNumOnScreen();
+});
+
+$('.func').on('click', function() {
+    var initialOperand = value;
+    var subsequentOperand = value;
+})
+
+$('#equalsButton').on('click', function() {
+  switch ($(".func").innerHTML) {
+    case "+":
+    	console.log("debugger")
+        sum(initialOperand, subsequentOperand);
+        break;
+    case "-":
+        subtract(initialOperand, subsequentOperand);
+        break;
+    case "/":
+        divide(initialOperand, subsequentOperand);
+        break;   
+    case "*":
+        multiply(initialOperand, subsequentOperand);
+        break; 
+    default:
+        displayNumOnScreen();
+    }
+})  
+
+// view
+function displayNumOnScreen() {
+  $("#screen")[0].innerHTML = value;
 }
-function multiply(a, b) {
-	return a*b;
-}
-function divide(a, b) {
-	return a/b;
-}
-
-
-
-function createMultiDigitNumbers() {
-	var input = document.getElementsByClassName("numButton").innerHTML;
-	fullCalcNum += input;
-}
-
-document.getElementById("screen").innerHTML = fullCalcNum;
-
-// if document.getElementsByClassName("funcButton")[0]
-// 	multiply()
